019f04117d24f5a26fb48ca9b9a51805bc3256cd
Jesus Arrioja <Jarrioja2210@gmail.com>
2023-11-14 18:16:58 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+""" 
+Desafío 01
+Dadas cuatro variables con diferentes textos, genera 
+una nueva variable con el siguiente contenido:
+
+“Python es un lenguaje de programación versátil”.
+"""
+
+cadena_1  = "versátil"
+cadena_2  = "Python"
+cadena_3  = "es un lenguaje"
+cadena_4  = "de programación"
+
+cadena_completa = f"\"{cadena_2} {cadena_3} {cadena_4} {cadena_1}\""
+
+print(cadena_completa)
